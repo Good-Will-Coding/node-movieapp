a8df3e6eaeffac0f2bd6b812a3531e0172459cb0
billy <billyliadphillips@gmail.com>
2019-03-01 16:17:02 +0000

    set up index and port

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const PORT = process.env.port || 3000;
+app.listen(PORT);
