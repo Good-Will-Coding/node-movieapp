3bd2c78e5238058b7636389fe2af4bdb11c95fbf
billy <billyliadphillips@gmail.com>
2019-03-02 16:21:40 +0000

    added axios and proxy middleware

--- client/src/components/ItemsBody.js
+++ client/src/components/ItemsBody.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 import ListItems from "./ListItems";
 import ListItem from "./ListItem";
+import axios from "axios";
 
 class ItemBody extends Component {
+  state = {
+    loading: true,
+    todo: {}
+  };
+
+  // todosLoading = () => {
+  //   if (this.state.loading) {
+  //     return <h1>Loading...</h1>;
+  //   }
+  // };
+
+  componentDidMount() {
+    axios.get("/api/todo").then(response => {
+      console.log(response);
+      this.setState({
+        loading: false,
+        todo: response.data
+      });
+    });
+  }
+
   render() {
     return (
       <ListItems>
--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TopNav = ({ title }) => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-dark mb-4">
     <span className="navbar-brand">{title}</span>
     </nav>
   )
