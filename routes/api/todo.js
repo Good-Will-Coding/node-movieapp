69a0604d38b6c466209553736a43b6652add257a
billy <billyliadphillips@gmail.com>
2019-03-02 10:18:40 +0000

    added in put/patch/delete

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -6,6 +6,7 @@ router.get("/", (req, res) => {
   // to execute query, use .then
   db.select()
     .from("todo")
+    .orderBy("id")
     .then(data => {
       res.send(data);
     });
@@ -14,8 +15,52 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   // INSERT INTO db
   db.insert(req.body)
+    // returning everything to not make second query
     .returning("*")
     .into("todo")
+    .then(data => {
+      res.send(data);
+    });
+});
+
+router.patch("/:id", (req, res) => {
+  // select * from todo where id = ourid
+  db("todo")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .returning("*")
+    .then(data => {
+      res.send(data);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  // select * from todo where id = ourid
+  db("todo")
+    .where({ id: req.params.id })
+    .update({
+      title: req.body.title || null,
+      is_done: req.body.is_done || null
+    })
+    .returning("*")
+    .then(data => {
+      res.send(data);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("todo")
+    .where({ id: req.params.id })
+    .del()
+    .then(() => {
+      res.json({ success: true });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("todo")
+    .where({ id: req.params.id })
+    .select()
     .then(data => {
       res.send(data);
     });
